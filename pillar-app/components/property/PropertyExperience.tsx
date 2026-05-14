bf2b5eb71021b25d2ebb459cb2742bc45c5b2f04
Cameron <cjrothai@gmail.com>
2026-05-14 18:47:06 -0400

    error pushing

--- pillar-app/components/property/PropertyExperience.tsx
+++ pillar-app/components/property/PropertyExperience.tsx
@@ -28,9 +28,9 @@ function MoonIcon() {
   );
 }
 
-function ChevronDown({ className }: { className?: string }) {
+function ChevronDown({ className, style }: { className?: string; style?: React.CSSProperties }) {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} aria-hidden="true">
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} style={style} aria-hidden="true">
       <path d="M6 9l6 6 6-6" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
